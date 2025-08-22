0a5e8670a2fbe67fae25d4207738f8656514cc90
FrogIn-Dev <frogindev@gmail.com>
2025-08-22 13:39:09 +0200

    Home page start designed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {RouterLink} from '@angular/router';
+import {LoginComponent} from '../login/login.component';
+import {RegisterComponent} from '../register/register.component';
+import { Dialog } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  authService = inject(AuthService);
+  private dialog = inject(Dialog);
 
+  openLogin() {
+    this.dialog.open(LoginComponent, {
+      panelClass: 'auth-dialog',
+      backdropClass: 'auth-backdrop',
+      disableClose: false,
+    });
+  }
+
+  openRegister() {
+    this.dialog.open(RegisterComponent, {
+      panelClass: 'auth-dialog',
+      backdropClass: 'auth-backdrop',
+      disableClose: false
+    });
+  }
 }
